861ba248c010aa824fff16ad212e08e6272cc680
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-01-14 09:13:14 +0100

    updated dataset structure and initial dataset prep space

--- experiment/main.py
+++ experiment/main.py
@@ -4,17 +4,19 @@ from pprint import pprint
 from secret import sekrit_key
 import time
 
+API_KEY = sekrit_key if sekrit_key else ""
+
 LLM_MODEL = ["openai/gpt-4", "openai/gpt-4o", "openai/gpt-4o-mini", "openai/gpt-3.5-turbo"]
-lm = dspy.LM(LLM_MODEL[-1], max_tokens=2000, api_key=sekrit_key)
+lm = dspy.LM(LLM_MODEL[-1], max_tokens=2000, api_key=API_KEY)
 dspy.configure(lm=lm)
 
-with open("dataset/MultiArith/MultiArith.json") as file:
+with open("dataset/zero-shot_cot/MultiArith/MultiArith.json") as file:
     data = json.load(file)
 
 list_hit = []
 total_N = 0
-frm = 100
-to = 200
+frm = 150
+to = 159
 ans_format = ": int"
 module = dspy.ChainOfThought(f"question -> answer{ans_format}")
 for datum in data[frm:to]:
@@ -22,10 +24,11 @@ for datum in data[frm:to]:
     label = datum["lSolutions"][0]
 
     res = module(question=question)
-    pred = res.get("answer")
+    pred = res.answer
     if label == pred:
         list_hit.append(pred)
     
-    time.sleep(0.1)
+    time.sleep(0.2)
+    print(f"done prompt #{len(list_hit)}")
 
 print(f"Accuracy is: {len(list_hit)/len(data[frm:to]) * 100} %")
