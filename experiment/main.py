eef43e54eef8cdb3b8c7c7da712399826ace3e91
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-01-15 17:01:49 +0100

    Restructures signature creation to enable customisation of prompt constraints

--- experiment/main.py
+++ experiment/main.py
@@ -11,42 +11,62 @@ API_KEY = secret if secret else ""
 LLM_MODEL = ["openai/gpt-4", "openai/gpt-4o", "openai/gpt-4o-mini", "openai/gpt-3.5-turbo"]
 
 
-@dataclass
-class SignatureZeroShotCoT:
-    """Class for signature specification"""
-    context: str = None
-    question: str = "question"
-    intermediate: str = "reasoning"
-    answer: str = "answer"
-    answer_hint: str = ""
+# @dataclass
+# class SignatureZeroShotCoT:
+#     """Class for signature specification"""
+#     context: str = None
+#     question: str = "question"
+#     intermediate: str = "reasoning"
+#     answer: str = "answer"
+#     answer_hint: str = ""
 
-#! TODO: SIGNATURE CLASS MET EXPLICIETE REASONING STATEMENT 
+
+
+class Reasoning1(dspy.Signature):
+    """"""
+    query = dspy.InputField(prefix="q") 
+    output = dspy.OutputField(desc="Let's think step by step.", prefix="a")
+
+class ReasoningMisleading(dspy.Signature):
+    """"""
+    query = dspy.InputField() 
+    output = dspy.OutputField(desc="By using the fact that the Earth is round")
+
+class ReasoningIrrelevant(dspy.Signature):
+    """"""
+    query = dspy.InputField() 
+    output = dspy.OutputField(desc="Bing bing wahooooo! Avada Kadavra")
+
+class ConclusionSig(dspy.Signature):
+    """We want a short answer without descriptive words"""
+    query = dspy.InputField()
+    output = dspy.OutputField(desc="Therefore, the answer is")
+
 
 def main():
     chosen_model = LLM_MODEL[0]
     data = load_dataset(file_path=None)
-    sig = SignatureZeroShotCoT(answer_hint=": int")
-    # sig = SignatureZeroShotCoT(question="prompt", answer="conclusion")
     
     lm = dspy.LM(chosen_model, max_tokens=2000, api_key=API_KEY)
     dspy.configure(lm=lm)
     
-    first_step = dspy.Predict(f"{sig.question} -> {sig.intermediate}")
-    second_step = dspy.Predict(f"{sig.intermediate} -> {sig.answer}{sig.answer_hint}")
+    # first_step = dspy.Predict(f"{sig.question} -> {sig.intermediate}")
+    first_step = dspy.Predict(Reasoning1)
+    second_step = dspy.Predict(ConclusionSig)
+    # second_step = dspy.Predict(f"{sig.intermediate} -> {sig.answer}{sig.answer_hint}")
     
-    for datum in data[110:113]:
+    for datum in data[210:211]:
         question = datum['sQuestion']
-        first_prompt = {sig.question: question}
         label = datum["lSolutions"][0]
 
-        intermediate = first_step(**first_prompt)
-        final_question = f"{question} \n\n{intermediate.reasoning}"
+        reasoning = first_step(query=question)
+        final_question = f"{question} \n\n{reasoning.output}"
         print(final_question)
 
-        second_prompt = {sig.intermediate: final_question}
-        prediction = second_step(**second_prompt)
-        print(f"=> {label = } \n{prediction[sig.answer]}\n")
-
+        response = second_step(query=final_question)
+        print(response.output)
+        print(f"{label = }")
+        
 if __name__ == "__main__":
     main()
 
