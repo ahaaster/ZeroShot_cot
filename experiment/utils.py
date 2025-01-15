5b0362f5ac56c183a049ea099143531bc1afb5a0
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-01-15 23:10:55 +0100

    added support to read csv files

--- experiment/utils.py
+++ experiment/utils.py
@@ -1,10 +1,14 @@
 import json
+import pandas as pd
 from pathlib import Path
 from typing import Optional, Union
 
 Num = Union[int, float]
 
 def load_dataset(file_path:[Path, str]):
-    file_path = Path("dataset/zero-shot_cot/MultiArith/MultiArith.json") if file_path is None else file_path
-    with open(file_path) as file:
-        return json.load(file)
+    file_path = Path("dataset/zero-shot_cot/MultiArith/MultiArith.json") if file_path is None else Path(file_path)
+    if file_path.suffix == ".json":
+        with open(file_path) as file:
+            return json.load(file)
+    elif file_path.suffix == ".csv":
+        return pd.read_csv(file_path)
