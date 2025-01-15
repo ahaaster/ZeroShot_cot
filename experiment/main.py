dc402e75b6da96b02c8e0bcb9c5dc8565338b1b3
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-01-15 17:02:28 +0100

    removed ancient code

--- experiment/main.py
+++ experiment/main.py
@@ -69,36 +69,3 @@ def main():
         
 if __name__ == "__main__":
     main()
-
-
-""" OLD CODE
-data = load_dataset()
-    
-    LLM_MODEL = ["openai/gpt-4", "openai/gpt-4o", "openai/gpt-4o-mini", "openai/gpt-3.5-turbo"]
-    lm = dspy.LM(LLM_MODEL[0], max_tokens=2000, api_key=API_KEY)
-    dspy.configure(lm=lm)
-
-    list_hit = []
-    total_N = 0
-    frm = 150
-    to = 152
-    # ans_format = ": int"
-    # module = dspy.ChainOfThought(f"question -> answer{ans_format}")
-    module = dspy.Predict(f"question -> answer: int")
-    for datum in data[frm:to]:
-        question = datum['sQuestion']
-        label = datum["lSolutions"][0]
-
-        # questions = [question, question, question]
-
-        res = module(question=question)
-        print(f"{question = } \n{label = } \n{res}")
-        # pred = res.answer
-        # if label == pred:
-        #     list_hit.append(pred)
-        
-        # time.sleep(0.2)
-        # print(f"done prompt #{len(list_hit)}")
-
-    # print(f"Accuracy is: {len(list_hit)/len(data[frm:to]) * 100} %")
-"""
