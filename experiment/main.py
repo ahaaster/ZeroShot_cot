2ab216d016b58fe87f92b06e05accaf61e19791d
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-01-16 12:26:01 +0100

    cleaned up, refactored, and further developed code. (Should've committed more in between)

--- experiment/main.py
+++ experiment/main.py
@@ -1,7 +1,5 @@
 import dspy
 import time
-# from typing import Optional
-# from dataclasses import dataclass
 from pathlib import Path
 
 from utils import Num, load_dataset
@@ -9,28 +7,6 @@ from secret import secret
 
 
 API_KEY = secret if secret else ""
-LLM_MODEL = ["openai/gpt-4", "openai/gpt-4o", "openai/gpt-4o-mini", "openai/gpt-3.5-turbo"]
-
-
-# @dataclass
-# class SignatureZeroShotCoT:
-#     """Class for signature specification"""
-#     context: str = None
-#     question: str = "question"
-#     intermediate: str = "reasoning"
-#     answer: str = "answer"
-#     answer_hint: str = ""
-
-class ChoiceReasoning(dspy.Signature):
-    """State the reasoning of arriving at a choice out of the given options"""
-    query = dspy.InputField(prefix="Q:")
-    choices = dspy.InputField(prefix="C:", format="List[str]") 
-    output = dspy.OutputField(desc="Let's think step by step.", prefix="A:")
-
-class ConclusionChoice(dspy.Signature):
-    """"""
-    query = dspy.InputField()
-    output = dspy.OutputField(desc="Therefore, among A through E, the answer is")
 
 class CoT(dspy.Module):
     """This is how DSPy's ChainOfThought module works"""
@@ -44,14 +20,8 @@ class CoT(dspy.Module):
         extended_signature = signature.prepend("reasoning", rationale_type, type_=str)
 
         self.predict = dspy.Predict(extended_signature, **config)
-        # # This modifies the signature from '*inputs -> *outputs' to '*inputs -> reasoning, *outputs'
-        # rationale_field = dspy.OutputField(prefix="Reasoning: Let's think step by step in order to")
-        # signature = dspy.Signature(signature).prepend_outputfield(rationale_field)
-
-        # self.predict = dspy.Predict(signature)
-    
+       
     def forward(self, **kwargs):
-        # Just forward the inputs to the sub-module
         return self.predict(**kwargs)
 
 class Reasoning(dspy.Module):
@@ -62,7 +32,7 @@ class Reasoning(dspy.Module):
         self.reason_prefix = "reasoning" 
 
         reasoning_field = dspy.OutputField(prefix=f"{self.reason_prefix}: {reasoning_hint}")
-        reasoning_sig = dspy.Signature(inpoets).append_output_field(reasoning_field)
+        reasoning_sig = dspy.Signature(f"{inpoets} ->").prepend(self.reason_prefix, reasoning_field, type_=str)
         self.query_gen = dspy.Predict(reasoning_sig)
         
         # Now construct the second signature
@@ -75,62 +45,55 @@ class Reasoning(dspy.Module):
         kwargs[self.reason_prefix] = query[self.reason_prefix]
         return self.predict(**kwargs)
 
-class Reasoning1(dspy.Signature):
-    """"""
-    query = dspy.InputField(prefix="q") 
-    output = dspy.OutputField(desc="Let's think step by step.", prefix="a")
 
-class ReasoningMisleading(dspy.Signature):
-    """"""
-    query = dspy.InputField() 
-    output = dspy.OutputField(desc="By using the fact that the Earth is round")
-
-class ReasoningIrrelevant(dspy.Signature):
-    """"""
-    query = dspy.InputField() 
-    output = dspy.OutputField(desc="Avada Kadavra")
-
-class ConclusionSig(dspy.Signature):
-    """We want a short answer without descriptive words"""
-    query = dspy.InputField()
-    output = dspy.OutputField(desc="Therefore, the answer is")
-
-
-def main():
-    chosen_model = LLM_MODEL[0]
-    file_path = Path("dataset/zero-shot_cot/CommonsenseQA/data.csv")
+def main(chosen_model, method, file_path: Path):
     data = load_dataset(file_path=file_path)
     
     dataset = []
-    for label, question, choices in data.values[:4]:
-        dataset.append(
-            dspy.Example(
-                question=question,
-                answer=label,
-                choices=choices
-            ).with_inputs("question", "choices")
-        )
+    try:
+        for label, question, choices in data.values[:4]:
+            dataset.append(
+                dspy.Example(
+                    question=question,
+                    answer=label,
+                    choices=choices
+                ).with_inputs("question", "choices")
+            )
+        inpoets = "question, choices"
+    except: 
+        for label, question in data.values:
+            dataset.append(
+                dspy.Example(
+                    question=question,
+                    answer=label,
+                ).with_inputs("question")
+            )
+        inpoets = "question"
 
     lm = dspy.LM(chosen_model, max_tokens=2000, api_key=API_KEY)
     dspy.configure(lm=lm)
     
-    # first_step = dspy.Predict(ChoiceReasoning)
-    # second_step = dspy.Predict(ConclusionChoice))
-    prompter = CoT("question, choices -> answer")
-    
+    if method == "zero-shot":
+        prompter = dspy.Predict(f"{inpoets} -> answer")
+    elif method == "default":
+        prompter = CoT("question, choices -> answer")
+    else: 
+        prompter = Reasoning(
+            inpoets=inpoets, 
+            outputs="answer", 
+            reasoning_hint="Avadra Kadavra!"
+        )
 
     for query in dataset[2:3]:
-        # print(query)
-        # print()
-        print(query.inputs())
-
         response = prompter(**query.inputs())
         print(response)
         print(query.labels())
         print()
 
-    x = lm.inspect_history(n=1)
-    print(x)
+
+
+    # x = lm.inspect_history(n=1)
+    # print(x)
     
     # for _, row in data.iterrows():
     #     question = row["question"]
@@ -147,4 +110,13 @@ def main():
     #     print(f"{label = }")
         
 if __name__ == "__main__":
-    main()
+    LLM_MODEL = ["openai/gpt-4", "openai/gpt-4o", "openai/gpt-4o-mini", "openai/gpt-3.5-turbo"][-1]
+    METHODS = ["zero-shot", "default", "eigen"][-1]
+    # file_path = Path("dataset/zero-shot_cot/CommonsenseQA/data.csv")
+    file_path = Path("dataset/zero-shot_cot/StrategyQA/data.csv")
+    
+    main(
+        chosen_model=LLM_MODEL, 
+        method=METHODS, 
+        file_path=file_path
+    )
