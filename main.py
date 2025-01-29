9606651289623ee7d39a1b2a49c3ba814d62e377
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-01-30 00:10:28 +0100

    dataloading

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import dspy
+import pandas as pd
+from pathlib import Path
+from typing import Any
+from dspy import Example
 
 LOCAL_MODELS = ["llama3.2:1b", "deepseek-r1:1.5b"]
 
@@ -6,13 +10,38 @@ LOCAL_MODELS = ["llama3.2:1b", "deepseek-r1:1.5b"]
 def main():
     chosen_model = LOCAL_MODELS[0]
     lm = dspy.LM(
-        f"ollama_chat/{chosen_model}", api_base="http://localhost:11434", api_key=""
+        f"ollama_chat/{chosen_model}",
+        api_base="http://localhost:11434",
+        api_key="",
+        cache=False,
     )
 
     x = lm("Hello World!", tempature=0.7)
     y = lm(messages=[{"role": "user", "content": "Hello World!"}])
 
     print(f"{x}\n\n{y}")
+
+
+def get_datasets() -> list[Example]:
+    prepared_datasets = Path("dataset/cot").glob("**/data.csv")
+    prepared_datasets: list[Path] = sorted(prepared_datasets)
+    data, dataset_name = load_dataset(prepared_datasets[0])
+
+    label_name, *input_names = data.columns
+    return [Example(**row).with_inputs(*input_names) for _, row in data.iterrows()]
+
+
+def load_dataset(file_path: Path) -> (Any, str):
+    dir_name = get_dir_name(file_path)
+    if file_path.suffix == ".json":
+        with open(file_path) as file:
+            return json.load(file), dir_name
+    elif file_path.suffix == ".csv":
+        return pd.read_csv(file_path), dir_name
+
+
+def get_dir_name(file_path: Path) -> str:
+    return file_path.parent.stem
 
 
 if __name__ == "__main__":
