64c1c1f1bebddcfb3fc6008e75d2de88738007d9
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-01-29 10:56:44 +0100

    hello world to the local LLM

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import dspy
 
+LOCAL_MODELS = ["llama3.2:1b", "deepseek-r1:1.5b"]
+
 
 def main():
-    pass
+    chosen_model = LOCAL_MODELS[0]
+    lm = dspy.LM(
+        f"ollama_chat/{chosen_model}", api_base="http://localhost:11434", api_key=""
+    )
+
+    x = lm("Hello World!", tempature=0.7)
+    y = lm(messages=[{"role": "user", "content": "Hello World!"}])
+
+    print(x)
+    print()
+    print(y)
 
 
 if __name__ == "__main__":
