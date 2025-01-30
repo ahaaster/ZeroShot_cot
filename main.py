d5223ebfbe416f1fe9a9eebcc36d621eb1766026
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-01-30 15:27:58 +0100

    initial prompting and useless refactoring

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
-import dspy
-import pandas as pd
+# import dspy
+from json import load as read_json
+from pandas import read_csv
 from pathlib import Path
 from typing import Any, Iterable
-from dspy import Example
+from dspy import LM, Example, Evaluate
 
 LOCAL_MODELS = ["llama3.2:1b", "deepseek-r1:1.5b", "phi3.5", "gemma:2b", "qwen2.5:3b"]
 
 
 def main():
-    # chosen_model = LOCAL_MODELS[2]
-    # lm = dspy.LM(
-    #     f"ollama_chat/{chosen_model}",
-    #     api_base="http://localhost:11434",
-    #     api_key="",
-    #     # cache=False,
-    # )
+    chosen_model = LOCAL_MODELS[2]
+    lm = LM(
+        f"ollama_chat/{chosen_model}",
+        api_base="http://localhost:11434",
+        api_key="",
+    )
 
     # x = lm("Hello World!", tempature=0.7)
     # y = lm(messages=[{"role": "user", "content": "Hello World!"}])
     # print(f"{x}\n\n{y}")
 
     data_path = Path("cot/CommonsenseQA")
-    data = get_datasets(data_path)[:1]
+    dataset = get_datasets(data_path)
 
-    labels, inputs = get_labels_and_inputs(data)
+    labels, inputs = get_labels_and_inputs(dataset)
     print(f"{labels= } | {inputs= }")
+    for data in dataset[:3]:
+        print(data[inputs[0]])
+        resp = lm(
+            messages=[
+                {"role": "user", "content": f"{data[inputs[0]]}\n{data[inputs[1]]}"}
+            ]
+        )
+        print(resp)
 
 
 def get_labels_and_inputs(dataset: list[Example] | Example) -> (list[str], list[str]):
@@ -62,9 +70,9 @@ def load_dataset(file_path: Path) -> (Any, str):
     dir_name = get_dir_name(file_path)
     if file_path.suffix == ".json":
         with open(file_path) as file:
-            return json.load(file), dir_name
+            return read_json(file), dir_name
     elif file_path.suffix == ".csv":
-        return pd.read_csv(file_path), dir_name
+        return read_csv(file_path), dir_name
 
 
 def get_dir_name(file_path: Path) -> str:
