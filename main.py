9c585b62e7f8dd9c1159ecf44bb682539c78903f
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-01-30 09:33:22 +0100

    added more small LLM models

--- main.py
+++ main.py
@@ -4,31 +4,38 @@ from pathlib import Path
 from typing import Any
 from dspy import Example
 
-LOCAL_MODELS = ["llama3.2:1b", "deepseek-r1:1.5b"]
+LOCAL_MODELS = ["llama3.2:1b", "deepseek-r1:1.5b", "phi3.5", "gemma:2b", "qwen2.5:3b"]
 
 
 def main():
-    chosen_model = LOCAL_MODELS[0]
+    chosen_model = LOCAL_MODELS[2]
     lm = dspy.LM(
         f"ollama_chat/{chosen_model}",
         api_base="http://localhost:11434",
         api_key="",
-        cache=False,
+        # cache=False,
     )
 
     x = lm("Hello World!", tempature=0.7)
     y = lm(messages=[{"role": "user", "content": "Hello World!"}])
-
     print(f"{x}\n\n{y}")
 
+    data_path = Path("cot/MultiArith")
+    data = get_datasets(data_path)
+    print(data[:2])
 
-def get_datasets() -> list[Example]:
-    prepared_datasets = Path("dataset/cot").glob("**/data.csv")
-    prepared_datasets: list[Path] = sorted(prepared_datasets)
+
+def get_datasets(data_path: Path = None) -> list[Example]:
+    prepared_datasets: list[Path] = fetch_datasets(data_path)
     data, dataset_name = load_dataset(prepared_datasets[0])
 
     label_name, *input_names = data.columns
     return [Example(**row).with_inputs(*input_names) for _, row in data.iterrows()]
+
+
+def fetch_datasets(dir_path: str | Path = "") -> list[Path]:
+    file_path = Path("dataset") / dir_path
+    return sorted(file_path.glob("**/data.csv"))
 
 
 def load_dataset(file_path: Path) -> (Any, str):
