b440ff2a9cb23826ccecd1d1015cf5751147c142
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-02-08 11:30:43 +0100

    fixed multiple choice prep

--- dataset/cot/CommonsenseQA/dataprep.py
+++ dataset/cot/CommonsenseQA/dataprep.py
@@ -2,12 +2,14 @@
 Prepare the dataset for the prompt framework implementation in this file.
 Also construct the bespoke signature here that is used in the first phase of zero-shot CoT
 """
+
 import json
 import pandas as pd
 from pathlib import Path
 
-HERE = Path("dataset/zero-shot_cot/CommonsenseQA/")
+HERE = Path("dataset/cot/CommonsenseQA/")
 RAW_SET = HERE / "dev_rand_split.jsonl"
+
 
 def prepare_dataset():
     raw_df = pd.read_json(path_or_buf=RAW_SET, lines=True)
@@ -17,7 +19,7 @@ def prepare_dataset():
     df["label"] = raw_df["answerKey"]
     for idx, query in enumerate(nested_questions):
         df.at[idx, "question"] = query["stem"]
-        
+
         choice_list = []
         choices = query["choices"]
         for choice in choices:
@@ -25,9 +27,9 @@ def prepare_dataset():
             value = choice["text"]
             choice = f"{key}) {value}"
             choice_list.append(choice)
-        
-        df.at[idx, "choices"] = choice_list
-    
+
+        df.at[idx, "choices"] = ", ".join(choice_list)
+
     return df[["label", "question", "choices"]]
 
 
@@ -35,4 +37,3 @@ if __name__ == "__main__":
     result = prepare_dataset()
     df = pd.DataFrame(result)
     df.to_csv(HERE / "data.csv", index=False)
-
