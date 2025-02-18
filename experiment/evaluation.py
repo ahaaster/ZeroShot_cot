09f8d3173f997102a1c95b59e81c40c38a91fc9b
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-02-18 15:41:56 +0100

    made evaluate_metrics function and made it run through main

--- experiment/evaluation.py
+++ experiment/evaluation.py
@@ -4,8 +4,12 @@ from pathlib import Path
 from .dataset import Dataset
 from .utils import fetch_datasets
 
-path_dataset = Path("dataset/cot/MultiArith")
-label_path = fetch_datasets(path_dataset, file_name="data")
 
-df = pd.read_csv(label_path)
-df_label = df.loc[:, "label"]
+def evaluate_metrics():
+    path_dataset = Path("dataset/cot/MultiArith")
+    label_path: list = fetch_datasets(path_dataset, file_name="data")
+    df = pd.read_csv(label_path[0])
+    label_series = df.loc[:, "label"]
+
+    shuffled_series = label_series.sample(frac=1)
+    print(shuffled_series.head())
--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from pathlib import Path
 
 from experiment import Dataset, prompt_control
 from experiment.utils import fetch_datasets
+from experiment.evaluation import evaluate_metrics
 
+EXPERIMENTS = ["prompt", "metric_eval"]
+EXPERIMENT = EXPERIMENTS[1]
+
+# Prompt 'settings'
 LOCAL_MODELS = ["llama3.2:1b", "deepseek-r1:1.5b", "phi3.5", "gemma:2b", "qwen2.5:3b"]
 PROMPT_METHODS = {
     "control": prompt_control,
@@ -16,12 +21,21 @@ PROMPT_METHODS = {
 }
 
 
-def main():
-    method = "control"
-    chosen_model = LOCAL_MODELS[1]
-    chosen_datasets = Path("dataset/cot")
-    prompter = PROMPT_METHODS[method]
+def main(experiment: str):
+    if experiment == "prompt":
+        method = "control"
+        chosen_model = LOCAL_MODELS[1]
+        chosen_datasets = Path("dataset/cot")
+        run_prompts(method, chosen_model, chosen_datasets, record_results=False)
 
+    elif experiment == "metric_eval":
+        evaluate_metrics()
+
+
+def run_prompts(
+    method: str, chosen_model: str, chosen_datasets: Path, record_results: bool = False
+):
+    prompter = PROMPT_METHODS[method]
     lm = dspy.LM(
         f"ollama_chat/{chosen_model}",
         api_base="http://localhost:11434",
@@ -33,8 +47,8 @@ def main():
 
     for data_path in data_paths:
         dataset = Dataset(data_path)
-        prompter(lm, dataset, chosen_model)
+        prompter(lm, dataset, chosen_model, record_results)
 
 
 if __name__ == "__main__":
-    main()
+    main(EXPERIMENT)
