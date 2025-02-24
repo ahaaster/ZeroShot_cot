8965e819bf4e52bcfea2b7ad23fe0d74dfcab504
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-02-24 13:41:14 +0100

    placeholder functions for scoring metrics

--- experiment/evaluation.py
+++ experiment/evaluation.py
@@ -1,8 +1,22 @@
+import dspy
 import pandas as pd
 from pathlib import Path
+from dspy.evaluate import SemanticF1
 
 from .dataset import Dataset
 from .utils import fetch_datasets
+
+
+def exact_match(resp: list[str], ground_truth: list[str]) -> float:
+    pass
+
+
+def decoding_score(resp: list[str], ground_truth: list[str]) -> float:
+    pass
+
+
+def semantic_scoreing(resp: list[str], ground_truth: list[str]) -> float:
+    pass
 
 
 def evaluate_metrics():
