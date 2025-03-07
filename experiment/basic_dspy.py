796d2290e5c22dc1c8e8fb3354e71eddf2d6f9ab
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-03-07 12:26:32 +0100

    responses and scores are now saved and arguments are adjusted to match other functionalities

--- experiment/basic_dspy.py
+++ experiment/basic_dspy.py
@@ -5,38 +5,45 @@ from dspy import LM, Evaluate, Predict
 from dspy.evaluate.metrics import answer_exact_match, answer_passage_match
 
 from .dataset import Dataset
-from .utils import save_results
-from .evaluation import Metric, Decoder, exact_match, exact_match_lower
+from .utils import save_results, save_score
+from .evaluation import Metric, Decoder
 
 
-def basic_dspy(dataset: Dataset, *, record_results: bool = False, **kwargs):
-    # Check if results have been recorded already
-    results_dir = Path("results/basic") / dataset.name
-    if results_dir.exists():
-        return
-
+def basic_dspy(
+    dataset: Dataset, *, metric_name: str, record_results: bool = False, **kwargs
+):
+    method = "basic"
     output_name = "answer"
     signature = f"{dataset.get_input_names()} -> {output_name}"
     prompter = Predict(signature=signature)
 
-    decoder = Decoder(answer_format="mc", greedy_first=False)
-    metric = Metric(exact_match, dataset.label_name, output_name, decoder)
+    decoder = Decoder(dataset.answer_format, greedy_first=False)
+    metric = Metric(metric_name, dataset.label_name, output_name, decoder)
 
     prompt_that_shit = Evaluate(
-        devset=dataset[:10],
+        devset=dataset,
         metric=metric,
-        num_threads=8,
+        num_threads=16,
         display_progress=True,
-        display_table=True,
-        return_all_scores=True,
+        # display_table=True,
+        # return_all_scores=True,  # return_outputs geeft al all_scores inbegrepen
         return_outputs=True,
         provide_traceback=True,
     )
 
-    avg_score, results, scores = prompt_that_shit(prompter)
+    avg_score, results = prompt_that_shit(prompter)
 
     if not record_results:
         return
 
     results_df = pd.DataFrame(results)
+    results_dir = Path("results") / method / dataset.name
+
     save_results(results_df, results_dir, dataset.model_name)
+    save_score(
+        score=avg_score,
+        model_name=dataset.model_name,
+        method=method,
+        metric_name=metric_name,
+        dataset_name=dataset.name,
+    )
