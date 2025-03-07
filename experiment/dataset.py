d694518fd7eacdbadbbff3595abe46d3cf09fb2a
Arnaud van Haaster <a.a.van.haaster@umail.leidenuniv.nl>
2025-03-07 12:21:17 +0100

    added new attribute to Dataset class

--- experiment/dataset.py
+++ experiment/dataset.py
@@ -14,11 +14,13 @@ class Dataset:
     label_name: str = None
     input_names: list[str] = None
     name: str = field(init=False)
+    answer_format: str = field(init=False)
     dataset: list[Example] = field(init=False)
 
     def __post_init__(self) -> None:
         self.name = get_dir_name(self.source_path)
         self.dataset = self._init_dataset()
+        self.answer_format = self._init_ans_format()
 
     def __iter__(self) -> Example:
         for dspy_example in self.dataset:
@@ -36,6 +38,16 @@ class Dataset:
         self.label_name, *inputs = data.columns
         self.input_names = inputs
         return [Example(**row).with_inputs(*inputs) for _, row in data.iterrows()]
+
+    def _init_ans_format(self) -> str:
+        formats = {
+            "CommonsenseQA": "mc",
+            "GSM8K": "number",
+            "MultiArith": "number",
+            "ObjectTracking": "text",
+            "StrategyQA": "boolean",
+        }
+        return formats[self.name]
 
     def get_input_names(self, concat_str: str = ", ") -> str:
         return concat_str.join(self.input_names)
